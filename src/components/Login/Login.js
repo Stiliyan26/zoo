e42076f273e7eded1133d85d0497b0963b5a0f4e
Stiliyan26 <stili262002@abv.bg>
2022-07-13 00:10:30 +0300

    implement login and register page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,29 +3,71 @@ import styles from './Login.module.css';
 import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const onLoginHandler = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const { email, password } = Object.fromEntries(formData);
+
+        console.log(email);
+        console.log(password);
+    }
+
     return (
         <div className={styles.center}>
             <h1>Login</h1>
-
-            <form method='POST'>
+            <form onSubmit={onLoginHandler} method="POST">
                 <div className={styles.txt_field}>
+                    <input type="text" name="email" />
+                        <span></span>
+                        <label>Email</label>
+                </div>
+                <div className={styles.txt_field}>
+                    <input type="password" name="password" />
+                        <span></span>
+                        <label>Password</label>
+                </div>
+
+                <input type="submit" value="Login" />
+                    <div className={styles.signup_link}>
+                        Not a member? <Link to="/register">Signup</Link>
+                    </div>
+            </form>
+        </div>
+    );
+
+    /*const onLogin = (e) => {
+        e.preventDefault();
+
+        const formData = Object.fromEntries(new FormData(e.currentTarget));
+        console.log(formData);
+    }
+
+    return (
+        <div className={styles.styles.center}>
+            <h1>Login</h1>
+
+            <form onSubmit={onLogin} method="POST">
+                <div className={styles.styles.txt_field}>
                     <input type="text" name="email" />
                     <label>Email</label>
                 </div>
 
-                <div className={styles.txt_field}>
+                <div className={styles.styles.txt_field}>
                     <input type="password" name="password" />
                     <label>Password</label>
                 </div>
 
-                <input type="submit" value="Login"/>
+                <input type="submit" value="Login" />
 
-                <div className={styles.signup_link}>
-                   Not a member? <Link to="/register"> Sign up </Link>
+                <div className={styles.styles.signup_link}>
+                    Not a member? <Link to="/register"> Sign up </Link>
                 </div>
             </form>
         </div>
     );
+    */
 }
 
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,30 +3,29 @@ import styles from './Register.module.css';
 import { Link } from "react-router-dom";
 
 const Register = () => {
+
     return (
         <div className={styles.center}>
             <h1>Register</h1>
-
-            <form method='POST'>
+            <form method="POST">
                 <div className={styles.txt_field}>
                     <input type="text" name="email" />
+                    <span></span>
                     <label>Email</label>
                 </div>
-
                 <div className={styles.txt_field}>
                     <input type="password" name="password" />
+                    <span></span>
                     <label>Password</label>
                 </div>
-
                 <div className={styles.txt_field}>
                     <input type="password" name="repass" />
+                    <span></span>
                     <label>Repeat Password</label>
                 </div>
-
-                <input type="submit" value="Login"/>
-
+                <input type="submit" value="Register" />
                 <div className={styles.signup_link}>
-                   Not a member? <Link to="/register"> Sign up </Link>
+                    Already have an account? <Link to="/login">Login in</Link>
                 </div>
             </form>
         </div>
